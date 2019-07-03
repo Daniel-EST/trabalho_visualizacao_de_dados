8ef3f1187994315793c0bd16702c3b60a39cd3b5
Daniel-EST <dd_santos@id.uff.br>
2019-07-03 10:52:04 -0300

    finished

--- src/zona_da_feira-BA.R
+++ src/zona_da_feira-BA.R
@@ -1,10 +1,8 @@
 require(ggplot2)
-require(ggthemr)
 
-data = read.csv2('../datasets/nascidos_zona_da_feira.csv')
+data = read.csv('../datasets/nascidos_zona_da_feira.csv')
 
 data$Mes = factor(data$Mes,levels = c("Março", "Dezembro"))
- 
-ggthemr("dust")
+
 ggplot(data, aes(fill=Mes, y=Nascimentos, x=Ano)) + 
   geom_bar(position="dodge", stat="identity")
